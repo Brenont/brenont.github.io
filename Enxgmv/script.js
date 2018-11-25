6bd6569bdfa824c2589964fa1f52cc75e33ff941
Breno Nunes <Brenontavares@gmail.com>
2018-11-24 23:44:24 -0200

    Hide container when key

--- Enxgmv/script.js
+++ Enxgmv/script.js
@@ -1,10 +1,28 @@
 var input = document.querySelector("#key");
 
-input.addEventListener("input", function(){
+var lengthKeyStepOne = 6;
+var firstKey = "123456";
+
+input.addEventListener("input", function () {
     var inputValue = input.value;
-    if(inputValue.length = 6){
-        if(input.value == "123456"){
+
+    if (inputValue.length = lengthKeyStepOne) {
+
+        if (input.value == firstKey) {
             console.error("ACERTOU MISERAVI");
+
+            var containerInputOne = document.querySelector(".container-step-one");
+
+            if (containerInputOne.classList.contains("show")) {
+
+                containerInputOne.classList.remove("show");
+                containerInputOne.classList.remove("content");
+
+                containerInputOne.classList.add("hideSlow");
+                setTimeout(function () {
+                    containerInputOne.classList.add("hide");
+                }, 1000);
+            }
         }
     }
 });
