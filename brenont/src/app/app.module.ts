4d9d34a59b2dff1f87dba4f0961c6ed90c142e3e
Breno Nunes <Brenontavares@gmail.com>
2019-03-12 00:50:12 -0300

    start project

---
+++ brenont/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MeComponent } from './components/me/me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorksComponent } from './components/works/works.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './containers/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MeComponent,
+    SkillsComponent,
+    WorksComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
