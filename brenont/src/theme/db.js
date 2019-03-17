416b50e6a3e3af711aec6748a39cba60297942bb
Breno Nunes <Brenontavares@gmail.com>
2019-03-17 01:11:41 -0300

    works section --WIP

--- brenont/src/theme/db.js
+++ brenont/src/theme/db.js
@@ -35,6 +35,10 @@ export const DATE = {
           value: "60%"
         },
         {
+          name: "Git",
+          value: "65%"
+        },
+        {
           name: "Jquery",
           value: "45%"
         },
@@ -42,6 +46,44 @@ export const DATE = {
     },
     works: {
       title: "Works",
+      works: [
+        {
+          title: "Aplicação Web Make Collection",
+          company: "Make Collection",
+          img: "../../../assets/img/works/make.png",
+          info: "Aplicação Web para a marca Make Collection.",
+          text: null,
+          date: "Fevereiro, 2019",
+          url: "http://makecollection.com.br/",
+        },
+        {
+          title: "Agendamento Rede D'Or São Luiz",
+          company: "Rede D'Or São Luiz",
+          img: "../../../assets/img/works/rede-dor.png",
+          info: "Sistema de agendamento para a empresa Rede D'Or São Luiz em Angular 2+.",
+          text: "Fiz parte da equipe de desenvolvimento responsável pelo front-end do sistema.",
+          date: "Novembro, 2018",
+          url: "https://servicos.rededor.com.br/",
+        },
+        {
+          title: "Site Estartando Devs",
+          company: "Estartando Devs",
+          img: "../../../assets/img/works/devs.png",
+          info: "O projeto consiste no desenvolvimento de uma aplicação web para o projeto Estartando Devs, no qual atuo.",
+          text: null,
+          date: "Agosto, 2018",
+          url: "http://estartandodevs.com.br/",
+        },
+        {
+          title: "Site General Goods",
+          company: "General Goods",
+          img: "../../../assets/img/works/general-goods.png",
+          info: "Aplicação Web desenvolvida para a empresa General Goods intermediado pelo Projeto/Curso Estartando Devs.",
+          text: null,
+          date: "Junho, 2018",
+          url: "http://www.generalgoods.com.br/",
+        },
+      ]
     }
   }
 
