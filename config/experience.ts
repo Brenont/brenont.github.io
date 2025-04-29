c86a9933714cd4464e3bb5b3a235bf88c03620bb
Breno Nunes <brenontavares@gmail.com>
2025-04-28 20:59:41 -0400

    feat: update feat ou beat experience --WIP

--- config/experience.ts
+++ config/experience.ts
@@ -34,7 +34,7 @@ export const Experiences: ExperienceInterface[] = [
     type: "Personal Project",
     category: ["Web Dev", "Full Stack"],
     shortDescription:
-      "Building a social platform for Musicians connect, create, and collaborate together",
+      "Leading the build of the platform from the start. At Feat ou Beat, Musicians can connect, create, and collaborate with others.",
     websiteLink: "https://featoubeat.com.br/",
     techStack: [
       "Next.js",
@@ -46,56 +46,47 @@ export const Experiences: ExperienceInterface[] = [
     ],
     startDate: new Date("2024-10-08"),
     endDate: new Date(),
-    companyLogoImg: "/experience/featoubeat/logo.png",
+    companyLogoImg: "/experience/featoubeat/logo.svg",
     pagesInfoArr: [
-      {
-        title: "Landing Page",
-        description:
-          "Modern and responsive landing page showcasing company services and portfolio",
-        imgArr: [
-          "/experience/builtdesign/landing_1.webp",
-          "/experience/builtdesign/landing_3.webp",
-          "/experience/builtdesign/landing_5.webp",
-          "/experience/builtdesign/landing_6.webp",
-          "/experience/builtdesign/landing_2.webp",
-          "/experience/builtdesign/landing_4.webp",
-        ],
-      },
-      {
-        title: "Custom PDF Reader and optimizer",
-        description:
-          "Specialized PDF viewer with optimization features for improved performance and user experience",
-        imgArr: ["/experience/builtdesign/pdf_opt.webp"],
-      },
-      {
-        title: "Clients Dashboard",
-        description:
-          "Comprehensive client portal with project tracking, document management, and communication tools",
-        imgArr: [
-          "/experience/builtdesign/cli_dashboard_1.webp",
-          "/experience/builtdesign/cli_dashboard_2.webp",
-          "/experience/builtdesign/cli_dashboard_3.webp",
-        ],
-      },
-      {
-        title: "Admin Dashboard",
-        description:
-          "Powerful administrative interface for managing users, projects, and system settings",
-        imgArr: ["/experience/builtdesign/logo.png"],
-      },
+      // {
+      //   title: "Landing Page",
+      //   description:
+      //     "Modern and responsive landing page showcasing company services and portfolio",
+      //   imgArr: [],
+      // },
+      // {
+      //   title: "Custom PDF Reader and optimizer",
+      //   description:
+      //     "Specialized PDF viewer with optimization features for improved performance and user experience",
+      //   imgArr: ["/experience/builtdesign/pdf_opt.webp"],
+      // },
+      // {
+      //   title: "Clients Dashboard",
+      //   description:
+      //     "Comprehensive client portal with project tracking, document management, and communication tools",
+      //   imgArr: [
+      //     // "/experience/builtdesign/cli_dashboard_1.webp",
+      //     // "/experience/builtdesign/cli_dashboard_2.webp",
+      //     // "/experience/builtdesign/cli_dashboard_3.webp",
+      //   ],
+      // },
+      // {
+      //   title: "Admin Dashboard",
+      //   description:
+      //     "Powerful administrative interface for managing users, projects, and system settings",
+      //   imgArr: ["/experience/builtdesign/logo.png"],
+      // },
     ],
     descriptionDetails: {
       paragraphs: [
-        "During my time at Builtdesign, I had the opportunity to work on a dynamic and user-focused project that involved designing and optimizing a website catering to a user base of over 4000 individuals. My role as a full-stack web developer was to ensure a seamless experience for users by creating an efficient and maintainable platform.",
-        "I collaborated closely with the product team to integrate cutting-edge features, employing technologies like Next.js and React with TypeScript for captivating front-end experiences. Additionally, I contributed significantly to the backend by utilizing Node.js, MongoDB, and GraphQL to design robust APIs and ensure smooth system functionality.",
-        "This experience allowed me to enhance my skills in various areas of web development and deliver a high-quality product. I gained proficiency in front-end technologies such as Material UI and Tailwind CSS, as well as backend technologies including Nest.js and MySQL. The project's success in catering to a large user base and providing an intuitive user interface has further motivated me to pursue excellence in web development.",
+        // "During my time at Builtdesign, I had the opportunity to work on a dynamic and user-focused project that involved designing and optimizing a website catering to a user base of over 4000 individuals. My role as a full-stack web developer was to ensure a seamless experience for users by creating an efficient and maintainable platform.",
+        // "I collaborated closely with the product team to integrate cutting-edge features, employing technologies like Next.js and React with TypeScript for captivating front-end experiences. Additionally, I contributed significantly to the backend by utilizing Node.js, MongoDB, and GraphQL to design robust APIs and ensure smooth system functionality.",
+        // "This experience allowed me to enhance my skills in various areas of web development and deliver a high-quality product. I gained proficiency in front-end technologies such as Material UI and Tailwind CSS, as well as backend technologies including Nest.js and MySQL. The project's success in catering to a large user base and providing an intuitive user interface has further motivated me to pursue excellence in web development.",
       ],
       bullets: [
-        "Developed and optimized a high-performing website catering to over 4000 users.",
-        "Collaborated closely with the product team to implement cutting-edge features.",
-        "Created an intuitive admin dashboard to efficiently manage and announce contest winners.",
-        "Leveraged Next.js, React with TypeScript for captivating front-end experiences.",
-        "Utilized Node.js, MongoDB, and GraphQL to design and manage databases.",
+        "Connecng the business side with the dev team.",
+        "Building scalable web applicaons for arst's networks.",
+        "Definion of technologies and project structure to have a scalable project.",
       ],
     },
   },
